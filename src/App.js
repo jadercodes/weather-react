e720929f6d068aa897286ba19f2e3a6157340fd5
jadercodes <97782554+jadercodes@users.noreply.github.com>
2022-05-29 13:54:58 +0700

    added contact details

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Search from "./Search";
+import Contact from "./Contact";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+      <Contact />
     </div>
   );
 }
---
+++ src/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Contact.css";
+
+export default function Contact() {
+  return (
+    <div className="Contact">
+      Open source code on{" "}
+      <a
+        className="git"
+        href="https://github.com/jadercodes/weather-react"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        GitHub
+      </a>
+    </div>
+  );
+}
